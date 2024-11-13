0b97170d14eb8a060a4d1f6207ab989d89508474
Miguel <164817944+Migue-22@users.noreply.github.com>
2024-11-13 12:04:17 -0600

    Add files via upload

---
+++ ProyectoPatronesDiseño/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using ProyectoPatronesDiseño.Models.Interface;
+
+namespace ProyectoPatronesDiseño.Models;
+
+public class Order
+{
+    public List<IFood> MealOrder { get; set; }
+
+    public Order()
+    {
+        MealOrder = new List<IFood>();
+    }
+
+}
